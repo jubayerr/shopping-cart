396e347352af721b693136ce1047a563837c7705
Showkat Jamil Jubayer <showkatjubayer@gmail.com>
2021-01-29 11:35:06 +0600

    modifed

--- index.js
+++ index.js
@@ -12,3 +12,18 @@ function inputValue(isIncrease) {
     const caseTotal = caseNewCount * 59
     document.getElementById('total-case').innerText = caseTotal;
 }
+
+function handlePhoneValue(isIncrease) {
+    const inputPhone = document.getElementById('input-phone');
+    const inputPhoneNumber = parseInt(inputPhone.value);
+    let phoneNewCount = 0;
+    if (isIncrease == true) {
+        phoneNewCount = inputPhoneNumber + 1;
+    }
+    if (isIncrease == false && inputPhoneNumber > 0) {
+        phoneNewCount = inputPhoneNumber - 1;
+    }
+    inputPhone.value = phoneNewCount;
+    const phoneTotal = phoneNewCount * 1219;
+    document.getElementById('total-phone').innerText = phoneTotal;
+}
